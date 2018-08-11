f749190d88ea8c5721a636a30003a98adcd1d22a
Massimiliano De Vivo <massimiliano.devivo>
2018-08-11 15:55:38 +0200

    wip

---
+++ lib/selenium_page/element/errors.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module SeleniumPage
+  # Page
+  class Element
+    # Errors
+    class Errors
+      # # AlreadyDefinedElementName
+      # class AlreadyDefinedElementName < StandardError
+      #   def initialize(element_name)
+      #     @element_name = element_name
+      #   end
+      #
+      #   def message
+      #     "The element '#{@element_name}' is already defined"
+      #   end
+      # end
+      #
+      # # SchemeAndAuthorityNotSet
+      # class SchemeAndAuthorityNotSet < StandardError
+      #   def message
+      #     'Please set scheme_and_authority for SeleniumPage' \
+      #     " with '.configure_scheme_and_authority'"
+      #   end
+      # end
+
+    end
+  end
+end
